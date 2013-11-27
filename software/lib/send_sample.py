0d72e1a4f0ad32a3f1472c6eafbdbfaf14e93f85
Bennett Rand <randb@engr.oregonstate.edu>
2013-11-27 13:55:46 -0800

    Added sample and trycommit functions

--- software/lib/send_sample.py
+++ software/lib/send_sample.py
@@ -1,18 +1,52 @@
 import sys
 import db_info
 import psycopg2
+import time
+
+measure_insert = "INSERT INTO measurement VALUES (DEFAULT, %s, %s, %s, %s) RETURNING id;"
+sample_insert = "INSERT INTO sample VALUES (DEFAULT, %s, %s, %s, %s);"
 
 class send_sample:
 	def __init__(self, n):
 		self.connections = []
+		self.cursors = []
+		
 		for x in xrange(n):
-			print "Opening", x
 			self.connections.append(psycopg2.connect(database = db_info.database, host = db_info.host, user = db_info.user, password = db_info.password))
 			
-	def __del__(self):
 		for c in self.connections:
-			print "Closing", c
-			c.close();
+			self.cursors.append(c.cursor())
+			
+		self.lastcommit = time.time()
+		self.nextcur = 0
+		
+	def __del__(self):
+		for c in self.cursors:
+			c.close()
+			
+		for c in self.connections:
+			c.commit()
+			c.close()
+			
+	def trycommit(self):
+		if time.time() - lastcommit > 1:
+			for c in self.connections:
+				c.commit()
+			lastcommit = time.time()
+			
+		
+	def sample(self, power, voltage, current, frequency, timefrom = time.asctime(), timetill = time.asctime()):
+		
+		self.cursors[self.nextcur].execute(measure_insert,(power, voltage, current, frequency))
+		mid = self.cursors[self.nextcur].fetchone()[0]
+		self.cursors[self.nextcur].execute(sample_insert,(timefrom, timetill, mid))
+		
+		self.nextcur += 1
+		
+		if self.nextcur >= len(self.cursors):
+			self.nextcur = 0;
+		
+		self.trycommit()
 
 def main(argc = len(sys.argv), args = sys.argv):
 	x = send_sample(4)
