eb6970124a3d2a7b1a51462bbaecf089a999e40e
Bennett Rand <randb@engr.oregonstate.edu>
2014-01-07 11:38:51 -0800

    Generator method, fonts

--- software/web/js/script.js
+++ software/web/js/script.js
@@ -1,4 +1,19 @@
-var the_body = getElementsByTagName("body")[0];
+var the_body = document.getElementsByTagName("body")[0];
 
-console.log(the_body);
+// console.log(the_body.innerHTML);
 
+function appendDevice(name)
+{
+	the_body.innerHTML += "<div class=\"device\"><h3>" + name + "</h3></div>";
+}
+
+appendDevice("Lorem");
+appendDevice("Ipsum");
+appendDevice("Dolor");
+appendDevice("Sit");
+appendDevice("Amet");
+
+for (var i=0;i<100;i++)
+{
+	appendDevice(i.toString());
+}
