59f01bef3195919157f1831fc992e6b0428fba19
Bennett Rand <randb@engr.oregonstate.edu>
2014-01-10 15:46:41 -0800

    Edited DOM correctly + Details opening

--- software/web/js/script.js
+++ software/web/js/script.js
@@ -1,19 +1,40 @@
 var devices = document.getElementById("devices");
+var details = document.getElementById("details");
 
 console.log(devices.innerHTML);
 
-function appendDevice(name)
+function appendDevice(name, id)
 {
-	devices.innerHTML += "<div class=\"device\"><h3>" + name + "</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque semper.</p></div>";
+	placeholder = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque semper.";
+	
+	link = document.createElement("a");
+	theDiv = document.createElement("div");
+	header = document.createElement("h3");
+	text = document.createElement("p");
+	
+	devices.appendChild(link);
+	link.appendChild(theDiv);
+	theDiv.appendChild(header);
+	theDiv.appendChild(text);
+	
+	link.setAttribute("href", "javascript:void(0)");
+	link.setAttribute("onClick", "detailsFor("+id.toString()+");");
+	
+	theDiv.setAttribute("class","device");
+	
+	header.appendChild(document.createTextNode(name));
+	
+	text.appendChild(document.createTextNode(placeholder));
 }
 
-appendDevice("Lorem");
-appendDevice("Ipsum");
-appendDevice("Dolor");
-appendDevice("Sit");
-appendDevice("Amet");
+function detailsFor(id)
+{
+	details.style.height = "50%";
+	console.log(id);
+	return;
+}
 
 for (var i=0;i<100;i++)
 {
-	appendDevice(i.toString());
+	appendDevice(i.toString(),i);
 }
