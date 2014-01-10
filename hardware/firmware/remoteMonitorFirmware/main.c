57b5df1a28668e869c3bbe4e41de1415769863bf
Kit Morton <mortonki@onid.oregonstate.edu>
2014-01-09 20:09:27 -0800

    Tested and debugged base station list maintenance and bacon packet parsing code. Should now be working.

--- hardware/firmware/remoteMonitorFirmware/main.c
+++ hardware/firmware/remoteMonitorFirmware/main.c
@@ -10,11 +10,14 @@
 #include "nwk.h"
 
 #include "uart.h"
+#include "protocol.h"
+#include "basestation.h"
 
 static NWK_DataReq_t txPacket;
-uint8_t uart_tx_buf[100];
-uint8_t uart_rx_buf[100];
-
+uint8_t uart_tx_buf[200];
+uint8_t uart_rx_buf[200];
+extern struct baseStation baseStationList[BASESTATION_LIST_SIZE];
+extern int8_t baseStationListLength;
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter" // Ignore the unused parameter warning here, the function has to have this prototype.
@@ -26,6 +29,20 @@ static void packetTxConf(NWK_DataReq_t *req) {
 #pragma GCC diagnostic pop
 
 static bool rfReceivePacket(NWK_DataInd_t *ind) {
+	// First figure out what kind of packet this is, and then call the appropreate function.
+	switch ((packetType_t) (ind->data[0])) {
+	case bacon:
+		processBaconPacket(ind);
+		for (int8_t cnt = 0; cnt < baseStationListLength; cnt++) {
+			printf("%u %16s\n", cnt, baseStationList[cnt].name);
+			printf("   Addr: %u\n   PAN ID: %u\n   rssi: %d\n", baseStationList[cnt].addr, baseStationList[cnt].PAN_ID, baseStationList[cnt].rssi);
+		}
+		printf("\n");
+		break;
+	default:
+		break;
+	}
+
 	return true;
 }
 
@@ -41,7 +58,7 @@ int main(void) {
 	NWK_OpenEndpoint(APP_ENDPOINT, rfReceivePacket);
 	PHY_SetTxPower(0);
 
-	uart_init_port(uart_baud_38400, uart_tx_buf, 100, uart_rx_buf, 100); // Init uart
+	uart_init_port(uart_baud_38400, uart_tx_buf, 200, uart_rx_buf, 200); // Init uart
 
 	// Configure onboard LED as output
 	DDRB |= 1<<4;
@@ -54,5 +71,6 @@ int main(void) {
 	PORTF &= ~(1<<2);
 
 	while (1) {
+		SYS_TaskHandler();
 	}
 }
