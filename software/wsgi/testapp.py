496f33ba6306c794ef8e62483afe8cf293e2fce2
Bennett Rand <randb@engr.oregonstate.edu>
2014-01-13 16:18:32 -0800

    Preparing for json data
    also, what is environ?

--- software/wsgi/testapp.py
+++ software/wsgi/testapp.py
@@ -1,9 +1,11 @@
+import json
+
 def application(environ, start_response):
-    status = '200 OK'
-    output = 'Hello World!'
-
-    response_headers = [('Content-type', 'text/plain'),
-                        ('Content-Length', str(len(output)))]
-    start_response(status, response_headers)
-
-    return [output]
+	status = '200 OK'
+	output = environ
+	
+	response_headers = [('Content-type', 'application/json'),
+						('Content-Length', str(len(output)))]
+	start_response(status, response_headers)
+	
+	return [output]
