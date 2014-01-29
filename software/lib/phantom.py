99e86b48c57c6a1cba40825e7ca78a6813cbd50e
Bennett Rand <randb@engr.oregonstate.edu>
2014-01-29 11:45:53 -0800

    Testing numpy

--- software/lib/phantom.py
+++ software/lib/phantom.py
@@ -1,8 +1,19 @@
+import sys
 import numpy
 
 samples = [0, 0.1, 0, 0, 0.1, 0, 0, 0.25, 0.75, 1, 1.5, 1.25, 1, 1, 1.1, 1, 0.9, 1, 0.9, 0.9, 0.8, 0.9, 0.5, 0.25, 0, 0, 0.1, 0, 0, 0.1]
 
+def get_phantom(a):
+	return
+
 def main():
+	numpy.percentile(samples, 0)
+	numpy.percentile(samples, 25)
+	numpy.percentile(samples, 50)
+	numpy.percentile(samples, 75)
+	numpy.percentile(samples, 100)
 	
-	
-if __name__ == "__main__"
+	return
+
+if __name__ == "__main__":
+	main()
