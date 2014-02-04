3b9105e375e689937297d8bd2826f77e87f2d22e
Kit Morton <mortonki@onid.oregonstate.edu>
2014-02-04 11:26:39 -0800

    Actually implemented LCD code finally. Can't test it yet due to lack of hardware. Will test later this week.

--- hardware/firmware/megaDrivers/lcd.c
+++ hardware/firmware/megaDrivers/lcd.c
@@ -1,11 +1,64 @@
 #include "lcd.h"
 
-void initLCD() {
+static lcd_cmd_t *lcd_buf;
+static uint8_t lcd_buf_start = 0;
+static uint8_t lcd_buf_end = 0;
+static uint8_t lcd_buf_size;
+
+void initLCD(lcd_cmd_t buffer[], uint8_t buf_len) {
 	// Init IO pins
 	DDRG |= (1<<2) | (1<<3);
-	DDRE = 0xFF; // Data port
+	PORTG |= (1<<2); // Clock should idle high because it is falling edge triggered.
 	DDRB |= (1<<5);
+	PORTB &= (1<<5); // Generally we want to be in write mode.
+	DDRE = 0xFF; // Set data port to outputs
 
-	PORTB &= (1<<5);
+	lcd_buf = buffer;
+	lcd_buf_size = buf_len;
+
+	sendLCDCmd(0x38); // Configure LCD for 8 bit mode with two lines of text
+	
+}
+
+void sendLCDCmd(uint8_t cmd) {
+	lcd_buf_end = (lcd_buf_end < (lcd_buf_size-1)) ? lcd_buf_end+1 : 0;
+	lcd_buf[lcd_buf_end].data = cmd;
+	lcd_buf[lcd_buf_end].command = true;
+}
+
+void writeChar(char c) {
+	lcd_buf_end = (lcd_buf_end < (lcd_buf_size-1)) ? lcd_buf_end+1 : 0;
+	lcd_buf[lcd_buf_end].data = c;
+	lcd_buf[lcd_buf_end].command = false;
+}
+
+void writeString(char* c) {
+	uint8_t charCnt = 0;
+	while (*c != 0) {
+		if (charCnt >= 16)
+			break; // Can't print more than 16 characters
+		if (charCnt >= 8)
+			LCD_MOVE_TO_CHAR(1,0); // move to beginning of second line if longer than 8 chars
+
+		writeChar(*c);
+		charCnt++;
+	}
+}
+
+void serviceLCD(void) {
+	if (lcd_buf_start != lcd_buf_end) {
+		if (lcd_buf[lcd_buf_start].command)
+			PORTG &= ~(1<<3);
+		else
+			PORTG |= (1<<3);
+
+		PORTE = lcd_buf[lcd_buf_start].data;
+		asm volatile("nop");
+		PORTG &= ~(1<<2); // Bring clock low for 1us
+		_delay_loop_2(4);
+		PORTG |= (1<<2);
+		
+		lcd_buf_start = (lcd_buf_start < (lcd_buf_size-1)) ? lcd_buf_start+1 : 0;
+	}
 }
 
--- hardware/firmware/megaDrivers/lcd.h
+++ hardware/firmware/megaDrivers/lcd.h
@@ -2,13 +2,25 @@
 #define LCD_H
 
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <string.h>
 
-void initLCD(void);
+#define LCD_CMD_CLEAR 0x01
+#define LCD_CMD_HOME 0x02
+#define LCD_CMD_DSP_OFF 0x08
+#define LCD_CMD_DSP_ON 0x0C
+#define LCD_MOVE_TO_CHAR(row,col) (sendLCDCmd(0x80 | (col +(row<<6))));
 
+typedef struct {
+	uint8_t data;
+	bool command;
+} lcd_cmd_t;
+
+void initLCD(lcd_cmd_t buffer[], uint8_t buf_len);
+void serviceLCD(void);
+void sendLCDCmd(uint8_t byte);
+void writeChar(char c);
+void writeString(char* c);
 
 #endif //LCD_H
