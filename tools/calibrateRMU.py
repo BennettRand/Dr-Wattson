285f7aa6b5123b2dc153f7444a317844e82686b7
Kit Morton <mortonki@onid.oregonstate.edu>
2014-02-15 16:46:38 -0800

    Finished implementing calibration script. Still needs to be properly tested with hardware, but it should work.

--- tools/calibrateRMU.py
+++ tools/calibrateRMU.py
@@ -9,26 +9,92 @@ import StringIO
 import math
 import csv
 
-print "Loading Calibration Firmware"
+def signExtend16(val):
+	if (val & 0x8000) :
+		return ((val^0xFFFF)+1)*-1
+	else :
+		return val
+
+print "Reading existing calibration/configuration information..."
+proc = subprocess.Popen(["avrdude", "-patmega256rfr2", "-Pusb", "-cavrisp2", "-q", "-q", "-Ueeprom:r:.oldcal.bin:r"], stdin=sys.stdin, stdout=sys.stdout)
+proc.wait()
+if (proc.returncode != 0):
+	print "Failed to connect to remote monitoring unit"
+	exit()
+
+oldconfig = open(".oldcal.bin", 'r')
+rawChunk = bytearray(oldconfig.read(8))
+macAddr = (rawChunk[0]) | (rawChunk[1] << 8) | (rawChunk[2] << 16) | (rawChunk[3] << 24) | (rawChunk[4]<<32) | (rawChunk[5]<<40) | (rawChunk[6]<<48) | (rawChunk[7]<<56)
+rawChunk = bytearray(oldconfig.read(2))
+old_voltageScaling1 = rawChunk[0] | (rawChunk[1]<<8)
+rawChunk = bytearray(oldconfig.read(2))
+old_voltageOffset1 = signExtend16(rawChunk[0] | (rawChunk[1]<<8))
+rawChunk = bytearray(oldconfig.read(2))
+old_currentScaling1 = signExtend16(rawChunk[0] | (rawChunk[1]<<8))
+rawChunk = bytearray(oldconfig.read(2))
+old_currentOffset1 = signExtend16(rawChunk[0] | (rawChunk[1]<<8))
+rawChunk = bytearray(oldconfig.read(2))
+old_voltageScaling2 = signExtend16(rawChunk[0] | (rawChunk[1]<<8))
+rawChunk = bytearray(oldconfig.read(2))
+old_voltageOffset2 = signExtend16(rawChunk[0] | (rawChunk[1]<<8))
+rawChunk = bytearray(oldconfig.read(2))
+old_currentScaling2 = signExtend16(rawChunk[0] | (rawChunk[1]<<8))
+rawChunk = bytearray(oldconfig.read(2))
+old_currentOffset2 = signExtend16(rawChunk[0] | (rawChunk[1]<<8))
+rawChunk = bytearray(oldconfig.read(2))
+old_linePeriodScaling = signExtend16(rawChunk[0] | (rawChunk[1]<<8))
+networkValid = False
+PAN_ID = 0
+Address = 0
+Name = ""
+rawChunk = bytearray(oldconfig.read(1))
+if (rawChunk[0] == 0xFF):
+	rawChunk = bytearray(oldconfig.read(5))
+	PAN_ID = rawChunk[0] | (rawChunk[1]<<8)
+	Address = rawChunk[3] | (rawChunk[4]<<8)
+	Name = oldconfig.read(16).strip().strip('\0')
+	networkValid = True
+
+oldconfig.close()
+
+print "Original Configuration Data:"
+print "    MAC Address:         " + '0x{0:08X}'.format(macAddr)
+print "    Voltage 1 Scaling:   " + str(old_voltageScaling1)
+print "    Voltage 1 Offset:    " + str(old_voltageOffset1)
+print "    Current 1 Scaling:   " + str(old_currentScaling1)
+print "    Current 1 Offset:    " + str(old_currentOffset1)
+print "    Voltage 2 Scaling:   " + str(old_voltageScaling2)
+print "    Voltage 2 Offset:    " + str(old_voltageOffset2)
+print "    Current 2 Scaling:   " + str(old_currentScaling2)
+print "    Current 2 Offset:    " + str(old_currentOffset2)
+print "    Line Period Scaling: " + str(old_linePeriodScaling)
+if (networkValid) :
+	print "    PAN_ID:              " + str(PAN_ID)
+	print "    Address:             " +str(Address)
+	print "    Name:                " + Name
+
+print "\nSaving current firmware..."
 # Read in the firmware from the device so we can reprogram it later.
 proc = subprocess.Popen(["avrdude", "-patmega256rfr2", "-Pusb", "-cavrisp2", "-q", "-q", "-Uflash:r:.oldFlash.hex:h"], stdin=sys.stdin, stdout=sys.stdout)
 proc.wait()
-#if (proc.returncode != 0) :
-#	exit()
+if (proc.returncode != 0) :
+	exit()
 
+print "Loading Calibration Firmware..."
 # program calibration firmware onto DUT
 proc = subprocess.Popen(["avrdude", "-patmega256rfr2", "-Pusb", "-cavrisp2", "-q", "-q", "-Uflash:w:calibrationTool.hex:h"], stdin=sys.stdin, stdout=sys.stdout)
 proc.wait()
-#if (proc.returncode != 0) :
-#	# If this failed, try to write the old firmware back and exit
-#	proc = subprocess.Popen(["avrdude", "-patmega256rfr2", "-Pusb", "-cavrisp2", "-q", "-q", "-Uflash:w:.oldFlash.hex:h"], stdin=sys.stdin, stdout=sys.stdout)
-#	proc.wait()
-#	exit()
+if (proc.returncode != 0) :
+	# If this failed, try to write the old firmware back and exit
+	proc = subprocess.Popen(["avrdude", "-patmega256rfr2", "-Pusb", "-cavrisp2", "-q", "-q", "-Uflash:w:.oldFlash.hex:h"], stdin=sys.stdin, stdout=sys.stdout)
+	proc.wait()
+	exit()
 
-portname = raw_input("Enter serial port name: ")
+print "Starting calibration procedure..."
+portname = raw_input("    Enter serial port name: ")
 try :
-	#ser = serial.Serial(portname, 115200, timeout = 1)
-	ser = open(os.path.expanduser(os.path.expandvars(portname.strip())), 'r+')
+	ser = serial.Serial(portname, 115200, timeout = 1)
+	#ser = open(os.path.expanduser(os.path.expandvars(portname.strip())), 'r+')
 except :
 	# If this failed, try to write the old firmware back and exit
 	proc = subprocess.Popen(["avrdude", "-patmega256rfr2", "-Pusb", "-cavrisp2", "-q", "-q", "-Uflash:w:.oldFlash.hex:h"], stdin=sys.stdin, stdout=sys.stdout)
@@ -36,16 +102,8 @@ except :
 	exit()
 
 # Collect calibration information
-V1offset = 0;
-I1offset = 0;
-V1scaling = 0;
-I1scaling = 0;
-V2offset = 0;
-I2offset = 0;
-V2scaling = 0;
-I2scaling = 0;
 
-yn = raw_input("Connect AC load to port 1. Press enter when ready (enter s to skip).")
+yn = raw_input("    Connect AC load to port 1. Press enter when ready (enter s to skip).")
 if not (yn.startswith('s')) :
 	#ser.write("s")
 	s = ser.readline()
@@ -54,8 +112,8 @@ if not (yn.startswith('s')) :
 		data += s
 		s = ser.readline()
 	
-	Vin = raw_input("Enter calibration voltage for port 1: ")
-	Iin = raw_input("Enter calibration current for port 1: ")
+	Vin = raw_input("    Enter calibration voltage for port 1: ")
+	Iin = raw_input("    Enter calibration current for port 1: ")
 	
 	reader = csv.reader(StringIO.StringIO(data))
 	Vsum = 0
@@ -70,12 +128,19 @@ if not (yn.startswith('s')) :
 		IsquareSum += int(row[3])*int(row[3])
 		rowcount += 1
 
-	V1offset = Vsum/rowcount
-	I1offset = Isum/rowcount
-	V1scaling = float(Vin)/math.sqrt(VsquareSum/rowcount)	
-	I1scaling = float(Iin)/math.sqrt(IsquareSum/rowcount)	
+	voltageOffset1 = int(round(Vsum/rowcount))
+	currentOffset1 = int(round(Isum/rowcount))
+	voltageScaling1 = int(round((float(Vin)/math.sqrt(VsquareSum/rowcount))*1000000))
+	currentScaling1 = int(round((float(Iin)/math.sqrt(IsquareSum/rowcount))*10000000))
+else:
+	voltageOffset1    = old_voltageOffset1
+	currentOffset1    = old_currentOffset1
+	voltageScaling1   = old_voltageScaling1
+	currentScaling1   = old_currentScaling1
 
-yn = raw_input("Connect AC load to port 2. Press enter when ready (enter s to skip).")
+
+
+yn = raw_input("    Connect AC load to port 2. Press enter when ready (enter s to skip).")
 if not (yn.startswith('s')) :
 	#ser.write("s")
 	s = ser.readline()
@@ -84,8 +149,8 @@ if not (yn.startswith('s')) :
 		data += s
 		s = ser.readline()
 	
-	Vin = raw_input("Enter calibration voltage for port 2: ")
-	Iin = raw_input("Enter calibration current for port 2: ")
+	Vin = raw_input("    Enter calibration voltage for port 2: ")
+	Iin = raw_input("    Enter calibration current for port 2: ")
 	
 	reader = csv.reader(StringIO.StringIO(data))
 	Vsum = 0
@@ -100,96 +165,104 @@ if not (yn.startswith('s')) :
 		IsquareSum += int(row[4])*int(row[4])
 		rowcount += 1
 	
-	V2offset = Vsum/rowcount
-	I2offset = Isum/rowcount
-	V2scaling = float(Vin)/math.sqrt(VsquareSum/rowcount)	
-	I2scaling = float(Iin)/math.sqrt(IsquareSum/rowcount)	
+	voltageOffset2 = int(round(Vsum/rowcount))
+	currentOffset2 = int(round(Isum/rowcount))
+	voltageScaling2 = int(round((float(Vin)/math.sqrt(VsquareSum/rowcount))*1000000))
+	currentScaling2 = int(round((float(Iin)/math.sqrt(IsquareSum/rowcount))*10000000))
+else:
+	voltageOffset2    = old_voltageOffset2
+	currentOffset2    = old_currentOffset2
+	voltageScaling2   = old_voltageScaling2
+	currentScaling2   = old_currentScaling2
 
 ser.close()
 
-
-print "Voltage 1 Offset: " + str(V1offset)
-print "Current 1 Offset: " + str(I1offset)
-print "Voltage 1 Scaling Factor: " + str(V1scaling)
-print "Current 1 Scaling Factor: " + str(I1scaling)
-print "Voltage 2 Offset: " + str(V2offset)
-print "Current 2 Offset: " + str(I2offset)
-print "Voltage 2 Scaling Factor: " + str(V2scaling)
-print "Current 2 Scaling Factor: " + str(I2scaling)
-
-macAddr = raw_input("Enter MAC address for remote monitoring unit: ")
-Name = raw_input("Enter default network name: ")
-PAN_IN = int(raw_input("Enter PAN ID for default network: "))
-Address = int(raw_input("Enter network address for default network base station: "))
-configFileName = os.path.expanduser(os.path.expandvars(raw_input("Enter file name for config file")))
-
-config = ConfigParser.RawConfigParser()
-configFileName = ""
-outputFileSpecified = False
-outputFileName = ".tempOutputFile.bin"
-program = False
-
 linePeriodScaling = 500
-networkValid = False
-calValid = True
 
-config.read(configFileName)
+print "\nCalibration Parameters:  Original Values:   New Values:"
+print "    Voltage 1 Offset:    " + str(old_voltageOffset1).ljust(19) +  str(voltageOffset1)
+print "    Current 1 Offset:    " + str(old_currentOffset1).ljust(19) +  str(currentOffset1)
+print "    Voltage 1 Scaling:   " + str(old_voltageScaling1).ljust(19) + str(voltageScaling1)
+print "    Current 1 Scaling:   " + str(old_currentScaling1).ljust(19) + str(currentScaling1)
+print "    Voltage 2 Offset:    " + str(old_voltageOffset2).ljust(19) +  str(voltageOffset2)
+print "    Current 2 Offset:    " + str(old_currentOffset2).ljust(19) +  str(currentOffset2)
+print "    Voltage 2 Scaling:   " + str(old_voltageScaling2).ljust(19) + str(voltageScaling2)
+print "    Current 2 Scaling:   " + str(old_currentScaling2).ljust(19) + str(currentScaling2)
+print "    Line Period Scaling: " + str(old_linePeriodScaling).ljust(19) + str(linePeriodScaling)
 
-try:
-	macAddr = config.get('Mac Address', 'address')
-except:
-	print("Missing MAC address field")
-	quit()
+yn = raw_input("Would you program these values (y/n)? ")
+if not (yn.lower().startswith("y")):
+	print "Reprogramming old firmware..."
+	proc = subprocess.Popen(["avrdude", "-patmega256rfr2", "-Pusb", "-cavrisp2", "-q", "-q", "-Uflash:w:.oldFlash.hex:h"], stdin=sys.stdin, stdout=sys.stdout)
+	proc.wait()
+	exit()
 
-try:
-	voltageScaling1 = int(config.get('Calibration Data', 'voltageScaling1'))
-	voltageOffset1 = int(config.get('Calibration Data', 'voltageOffset1'))
-	currentScaling1 = int(config.get('Calibration Data', 'currentScaling1'))
-	currentOffset1 = int(config.get('Calibration Data', 'currentOffset1'))
-	voltageScaling2 = int(config.get('Calibration Data', 'voltageScaling2'))
-	voltageOffset2 = int(config.get('Calibration Data', 'voltageOffset2'))
-	currentScaling2 = int(config.get('Calibration Data', 'currentScaling2'))
-	currentOffset2 = int(config.get('Calibration Data', 'currentOffset2'))
-	linePeriodScaling = int(config.get('Calibration Data', 'linePeriodScaling'))
-	calValid = True;
-	print("Calibration values found")
-except:
-	calValid = False;
-	print("No calibration values found")
+if networkValid:
+	new_macAddr = raw_input("    Enter MAC address for remote monitoring unit (default: "+'0x{0:08X}'.format(macAddr)+"): ")
+	if (new_macAddr.strip() != ""):
+		macAddr = new_macAddr.strip()
+	
+	new_PAN_ID = raw_input("    Enter PAN ID for default network (default: "+str(PAN_ID)+"): ")
+	if (new_PAN_ID.strip() != ""):
+		PAN_ID = int(PAN_ID.strip())
+	
+	new_Address = raw_input("    Enter network address for default network base station (default: "+str(Address)+"): ")
+	if (new_Address.strip() != ""):
+		Address = int(new_Address.strip())
 
-try:
-	PAN_ID = int(config.get('Network Information', 'PAN_ID'))
-	Address = int(config.get('Network Information', 'Address'))
-	Name = config.get('Network Information', 'Name')
-	networkValid = True
-	print("Network information found")
-except:
-	print("No network information found")
+	new_Name = raw_input(("    Enter default network name (default: "+Name)+"): ")
+	if (new_Name.strip() != ""):
+		Name = new_Name
+else:
+	macAddr = raw_input("    Enter MAC address for remote monitoring unit: ")
+	PAN_IN = int(raw_input("    Enter PAN ID for default network: "))
+	Address = int(raw_input("    Enter network address for default network base station: "))
+	Name = raw_input("    Enter default network name: ")
+configFileName = os.path.expanduser(os.path.expandvars(raw_input("    Enter file name for config file: ")))
 
-macAddr = re.sub(r"[^ABCDEF0123456789]", "", string.upper(macAddr))[:16]
-if len(macAddr) != 16:
-	print("Invalid MAC address")
-	quit()
-macAddr = int(macAddr,16);
+config = ConfigParser.SafeConfigParser()
 
+print "Writing Configuration File..."
+config.add_section("Mac Address")
+config.set("Mac Address","address",'{0:08X}'.format(macAddr))
+
+config.add_section("Calibration Data")
+config.set('Calibration Data', 'voltageScaling1',str(voltageScaling1))
+config.set('Calibration Data', 'voltageOffset1',str(voltageOffset1))
+config.set('Calibration Data', 'currentScaling1',str(currentScaling1))
+config.set('Calibration Data', 'currentOffset1',str(currentOffset1))
+config.set('Calibration Data', 'voltageScaling2',str(voltageScaling2))
+config.set('Calibration Data', 'voltageOffset2',str(voltageOffset2))
+config.set('Calibration Data', 'currentScaling2',str(currentScaling2))
+config.set('Calibration Data', 'currentOffset2',str(currentOffset2))
+config.set('Calibration Data', 'linePeriodScaling',str(linePeriodScaling))
+
+config.add_section("Network Information")
+config.set('Network Information', 'PAN_ID', str(PAN_ID))
+config.set('Network Information', 'Address', str(Address))
+config.set('Network Information', 'Name', str(Name))
+
+cfgFile = open(configFileName,'w')
+config.write(cfgFile)
+cfgFile.close()
+
+print "Programming new configuration..."
 while len(Name) < 16:
 	Name += "\0"
 Name = Name[:16]
 
-if not outputFileSpecified:
-	output_fd = open(".tempOutputFile.bin", 'w')
+output_fd = open(".outputFile.bin", 'w')
 
 dataArray = [macAddr & 0xFF, (macAddr>>8) & 0xFF, (macAddr>>16) & 0xFF, (macAddr>>24) & 0xFF, (macAddr>>32) & 0xFF, (macAddr>>40) & 0xFF, (macAddr>>48) & 0xFF, (macAddr>>56) & 0xFF]
-if (calValid):
-	dataArray += [voltageScaling1 & 0xFF, (voltageScaling1>>8) & 0xFF]
-	dataArray += [voltageOffset1 & 0xFF, (voltageOffset1>>8) & 0xFF]
-	dataArray += [currentScaling1 & 0xFF, (currentScaling1>>8) & 0xFF]
-	dataArray += [currentOffset1 & 0xFF, (currentOffset1>>8) & 0xFF]
-	dataArray += [voltageScaling1 & 0xFF, (voltageScaling1>>8) & 0xFF]
-	dataArray += [voltageOffset1 & 0xFF, (voltageOffset1>>8) & 0xFF]
-	dataArray += [currentScaling1 & 0xFF, (currentScaling1>>8) & 0xFF]
-	dataArray += [currentOffset1 & 0xFF, (currentOffset1>>8) & 0xFF]
-	dataArray += [linePeriodScaling & 0xFF, (linePeriodScaling >> 8) & 0xFF]
+dataArray += [voltageScaling1 & 0xFF, (voltageScaling1>>8) & 0xFF]
+dataArray += [voltageOffset1 & 0xFF, (voltageOffset1>>8) & 0xFF]
+dataArray += [currentScaling1 & 0xFF, (currentScaling1>>8) & 0xFF]
+dataArray += [currentOffset1 & 0xFF, (currentOffset1>>8) & 0xFF]
+dataArray += [voltageScaling2 & 0xFF, (voltageScaling2>>8) & 0xFF]
+dataArray += [voltageOffset2 & 0xFF, (voltageOffset2>>8) & 0xFF]
+dataArray += [currentScaling2 & 0xFF, (currentScaling2>>8) & 0xFF]
+dataArray += [currentOffset2 & 0xFF, (currentOffset2>>8) & 0xFF]
+dataArray += [linePeriodScaling & 0xFF, (linePeriodScaling >> 8) & 0xFF]
 databytearray = bytearray(dataArray)
 if (networkValid):
 	dataArray = [0xFF]
@@ -204,10 +277,14 @@ else:
 output_fd.write(databytearray)
 output_fd.close();
 
-if program:
-	proc = subprocess.Popen(["avrdude", "-patmega256rfr2", "-Pusb", "-cavrisp2", "-Ueeprom:w:"+outputFileName+":r"], stdin=sys.stdin, stdout=sys.stdout)
-	proc.wait()
+proc = subprocess.Popen(["avrdude", "-patmega256rfr2", "-Pusb", "-cavrisp2", "-Ueeprom:w:.outputFile.bin:r"], stdin=sys.stdin, stdout=sys.stdout)
+proc.wait()
 
-if not outputFileSpecified:
-	os.unlink(".tempOutputFile.bin")
+print "Flashing original Firmware"
+proc = subprocess.Popen(["avrdude", "-patmega256rfr2", "-Pusb", "-cavrisp2", "-q", "-q", "-Uflash:w:.oldFlash.hex:h"], stdin=sys.stdin, stdout=sys.stdout)
+proc.wait()
+
+os.unlink(".outputFile.bin")
+os.unlink(".oldFlash.hex")
+os.unlink(".oldcal.bin")
 
