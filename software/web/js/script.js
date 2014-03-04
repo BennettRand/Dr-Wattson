20225cac0a24c16923618004ff36486d1e1d0605
Bennett Rand <randb@engr.oregonstate.edu>
2014-03-03 20:04:46 -0800

    UI Tweaks and Preparing for DB

--- software/web/js/script.js
+++ software/web/js/script.js
@@ -8,43 +8,84 @@ $.jqplot.config.defaultHeight = ($(window).height()/2)-50;
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
-dList =	[{"name":"Foo","id":1},
-		{"name":"Bar","id":2},
-		{"name":"Baz","id":3},
-		{"name":"Qux","id":4},
-		{"name":"Quxom","id":5},
-		{"name":"Lorem","id":6},
-		{"name":"Ipsum","id":7},
-		{"name":"Dolor","id":8},
-		{"name":"Sit","id":9},
-		{"name":"Amet","id":10}];
+function drawChart(ele, id)
+{
+	$(ele).sparkline(powerFor(id),
+	{width: '100%',
+	height: '100%',
+	type: 'line',
+	lineColor: '#444',
+	fillColor: '#888',
+	spotColor: false,
+	minSpotColor: false,
+	maxSpotColor: false,
+	highlightSpotColor: false,
+	highlightLineColor: '#444',
+	chartRangeMin: 0,
+	chartRangeMax: 10,
+	chartRangeClip: true,
+	normalRangeColor: '#c0c0c0',
+	drawNormalOnTop: false});
+}
 
-function appendDevice(name, id)
+function appendDevice(name1, id1, name2, id2)
 {
 	placeholder = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque semper.";
 	
-	link = document.createElement("a");
-	theDiv = document.createElement("div");
-	header = document.createElement("h3");
-	text = document.createElement("p");
-	spark = document.createElement("div");
+	plug = document.createElement("div");
+	plug.setAttribute("class","plug");
+	plug.setAttribute("name1",name1);
+	plug.setAttribute("id1",id1);
+	plug.setAttribute("name2",name2);
+	plug.setAttribute("id2",id2);
+	devices.appendChild(plug);
 	
-	devices.appendChild(link);
-	link.appendChild(theDiv);
-	theDiv.appendChild(header);
-	theDiv.appendChild(spark);
-	//theDiv.appendChild(text);
 	
-	link.setAttribute("href", "javascript:void(0)");
-	link.setAttribute("onClick", "getDetailsFor(\""+name+"\", "+id.toString()+");");
+	link1 = document.createElement("a");
+	theDiv1 = document.createElement("div");
+	header1 = document.createElement("h3");
+	text1 = document.createElement("p");
+	spark1 = document.createElement("div");
 	
-	theDiv.setAttribute("class","device");
+	plug.appendChild(link1);
+	link1.appendChild(theDiv1);
+	theDiv1.appendChild(header1);
+	theDiv1.appendChild(spark1);
 	
-	header.appendChild(document.createTextNode(name));
+	link1.setAttribute("href", "javascript:void(0)");
+	link1.setAttribute("onClick", "getDetailsFor(\""+name1+"\", "+id1.toString()+");");
 	
-	text.appendChild(document.createTextNode(placeholder));
+	theDiv1.setAttribute("class","device");
 	
-	spark.setAttribute("class", "bgChart");
+	header1.appendChild(document.createTextNode(name1));
+	
+	text1.appendChild(document.createTextNode(placeholder));
+	
+	spark1.setAttribute("class", "bgChart");
+	drawChart(spark1, id1);
+	
+	link2 = document.createElement("a");
+	theDiv2 = document.createElement("div");
+	header2 = document.createElement("h3");
+	text2 = document.createElement("p");
+	spark2 = document.createElement("div");
+	
+	plug.appendChild(link2);
+	link2.appendChild(theDiv2);
+	theDiv2.appendChild(header2);
+	theDiv2.appendChild(spark2);
+	
+	link2.setAttribute("href", "javascript:void(0)");
+	link2.setAttribute("onClick", "getDetailsFor(\""+name2+"\", "+id2.toString()+");");
+	
+	theDiv2.setAttribute("class","device");
+	
+	header2.appendChild(document.createTextNode(name2));
+	
+	text2.appendChild(document.createTextNode(placeholder));
+	
+	spark2.setAttribute("class", "bgChart");
+	drawChart(spark2, id1);
 }
 
 function closeDetails()
@@ -166,10 +207,22 @@ function detailsFor(name, id, arr)
 
 function getDetailsFor(name, id)
 {
-	$.getJSON("/testapp/detail",{id:id}, function(data){
+	/*$.ajax({
+		dataType: "json",
+		url: "http://"+document.location.host+":8080/testapp/detail",
+		data:{id:id},
+		success: function(data){
+		detailsFor(name, id, data);
+		},
+		timeout: 10000,
+		cache: false,
+		crossDomain: true
+	});*/
+	$("#loading")[0].style.visibility="visible";
+	$.getJSON("http://"+document.location.host+":8080/detail",{id:id}, function(data){
 		
 		detailsFor(name, id, data);
-		
+		$("#loading")[0].style.visibility="hidden";
 	});
 }
 
@@ -195,53 +248,51 @@ function getTotals()
 	return {voltage:v,power:p,current:i};
 }
 
-for (var i=0;i<dList.length;i++)
+function drawSparks()
 {
-	appendDevice(dList[i].name,dList[i].id);
-}
-
-graphs = $(".bgChart");
-
-function drawCharts()
-{
-	for (i=0; i<graphs.length; i+=1)
+	dList =	[{"name1":"Foo","id1":1,"name2":"Bar","id2":2},
+		{"name1":"Baz","id1":3,"name2":"Qux","id2":4},
+		{"name1":"Quxom","id1":5,"name2":"Lorem","id2":6},
+		{"name1":"Ipsum","id1":7,"name2":"Dolor","id2":8},
+		{"name1":"Sit","id1":9,"name2":"Amet","id2":10},
+		{"name1":"Foo","id1":1,"name2":"Bar","id2":2},
+		{"name1":"Baz","id1":3,"name2":"Qux","id2":4},
+		{"name1":"Quxom","id1":5,"name2":"Lorem","id2":6},
+		{"name1":"Ipsum","id1":7,"name2":"Dolor","id2":8},
+		{"name1":"Sit","id1":9,"name2":"Amet","id2":10}];
+	
+	$("#loading")[0].style.visibility="visible";
+	plugs = $(".plug")
+	for (var p = 0; p < plugs.length; p++)
 	{
-		$(graphs[i]).sparkline(powerFor(dList[i].id),
-		{width: '100%',
-		height: '100%',
-		type: 'line',
-		lineColor: '#bbb',
-		fillColor: '#ddd',
-		spotColor: false,
-		minSpotColor: false,
-		maxSpotColor: false,
-		highlightSpotColor: false,
-		highlightLineColor: '#888',
-		chartRangeMin: 0,
-		chartRangeMax: 10,
-		chartRangeClip: true,
-		normalRangeColor: '#c0c0c0',
-		drawNormalOnTop: false});
+		// console.log(plugs[p]);
+		plugs[p].remove();
 	}
+	for (var i=0;i<dList.length;i++)
+	{
+		appendDevice(dList[i].name1,dList[i].id1,dList[i].name2,dList[i].id2);
+	}
+	$("#loading")[0].style.visibility="hidden";
 }
 
 function clockTick()
 {
 	var d=new Date();
-	var timeString = days[d.getDay()]+" ";
+	var timeString = "";
+	timeString += days[d.getDay()]+" ";
 	timeString += months[d.getMonth()]+" ";
 	timeString += d.getDate().toString()+" ";
-	timeString += d.getFullYear().toString()+" ";
+	// timeString += d.getFullYear().toString()+" ";
 	
 	var currentHour = d.getHours()%12;
 	if (currentHour == 0){currentHour = 12;}
 	timeString += (currentHour%12).toString()+":";
 	
-	if(d.getMinutes() < 10){timeString += "0"+d.getMinutes().toString()+":";}
-	else{timeString += d.getMinutes().toString()+":";}
+	if(d.getMinutes() < 10){timeString += "0"+d.getMinutes().toString()+" ";}
+	else{timeString += d.getMinutes().toString()+" ";}
 	
-	if(d.getSeconds() < 10){timeString += "0"+d.getSeconds().toString()+" ";}
-	else{timeString += d.getSeconds().toString()+" ";}
+	// if(d.getSeconds() < 10){timeString += "0"+d.getSeconds().toString()+" ";}
+	// else{timeString += d.getSeconds().toString()+" ";}
 	
 	if(d.getHours() < 12){timeString += "AM";}
 	else{timeString += "PM";}
@@ -260,10 +311,11 @@ function powerCounts()
 	$("#totalPower")[0].innerHTML = "Power: "+Math.round(tPower.power).toString()+" W";
 }
 
+drawSparks();
 powerCounts();
-drawCharts();
 clockTick();
 
 setInterval(clockTick,1000);
 setInterval(powerCounts,1000);
+setInterval(drawSparks,10000);
 // setInterval(drawCharts,1000);
